b136bc98c94e01da53a6ea364f23e834ffce3ffc
LukaszK <pabc1122@gmail.com>
2013-01-02 22:48:58 +0100

    Add sample tests.

---
+++ Tests/Demo/Demo01Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace QuickTestsFramework.Tests.Demo
+{
+   [TestFixture]
+   [Explicit]
+   public sealed class Demo01Tests
+   {
+      private Runner _runner;
+
+      [TestFixtureSetUp]
+      public void SetUp()
+      {
+         _runner = RunnerHelper.Create();
+         _runner.RunInitializers(this); // Run all tests in fixture. For each test will be run code that initialize some "state".
+
+         // At this place should be run a process that use "state".
+         Console.WriteLine("Process...");
+      }
+
+      [Test]
+      public void T01()
+      {
+         _runner.Run(
+            () => // this delegate will be called before the process called in SetUp method.
+            {
+               Console.WriteLine("T01: Initialize input state."); 
+               Trace.WriteLine("Trace T01: Initialize input state.");
+               // Output from Console, Trace and Debug will be catched and stored internal until assertion delegate start.
+               // Output of the test should look like there was no magic hidden underneath
+            },
+            () => // this delegate will be called after the procese called in SetUp method.
+            {
+               Console.WriteLine("T01: Analyze output state.");
+               Trace.WriteLine("Trace T01: Analyze output state.");
+            });
+      }
+
+      [Test]
+      public void T02()
+      {
+         _runner.Run(
+            () =>
+            {
+               Console.WriteLine("T02: Initialize input state.");
+               Debug.WriteLine("Debug T02: Initialize input state.");
+            },
+            () =>
+            {
+               Console.WriteLine("T02: Analyze output state.");
+               Assert.Fail("some reason...");
+            });
+      }
+   }
+}
---
+++ Tests/Demo/Demo02Tests.cs
@@ -0,0 +1,77 @@
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using NUnit.Framework;
+using QuickTestsFramework.NUnit;
+
+namespace QuickTestsFramework.Tests.Demo
+{
+   [TestFixture]
+   [Explicit]
+   public sealed class Demo02Tests
+   {
+      private Runner _runner;
+
+      [TestFixtureSetUp]
+      public void SetUp()
+      {
+         _runner = RunnerHelper.Create();
+         _runner.RunInitializers(this);
+
+         Console.WriteLine("Process...");
+      }
+
+      [Test]
+      public void T01()
+      {
+         _runner.Run(
+            () => new[] { new { A = 1, B = 2 }, new { A = 5, B = 7 }, new { A = 6, B = 9 } }, // Generate test cases. It starting before initializing delegate. 
+            tc =>
+            {
+               Console.WriteLine("Init T01: tc = {0}", tc);
+               // initialize state...
+            },
+            tc =>
+            {
+               Console.WriteLine("Verifi T01: tc = {0}", tc);
+               Assert.That(tc.A, Is.Not.EqualTo(5)); // simulate one assertion fail.
+            });
+      }
+
+      [Test]
+      public void T02()
+      {
+         var random = new Random();
+         _runner.Run(
+            () => Sequential.New(_ => new
+            {
+               A = _.OneOf(1, 3, 5, 8, 5),
+               B = _.OneOf(1, 4, 2, 8, 3),
+               C = "const",
+               D = random.Next(100)
+            }),
+            tc =>
+            {
+               Console.WriteLine("Init T01: tc = {0}", tc);
+            },
+            tc =>
+            {
+               Console.WriteLine("Verifi T01: tc = {0}", tc);
+               Assert.That(tc.A, Is.Not.EqualTo(5)); // simulate one assertion fail.
+            });
+      }
+
+      [Test]
+      [TraditionalTest] // this mean that this test run only in one stage like normal NUnit tests.
+      public void T03()
+      {
+         var multiAssert = new MultipleAssertion(new NUnitExceptionFilter(), new NUnitAssertionAction());
+
+         multiAssert.Run( // all assertion will run.
+            () => Assert.True(true),
+            () => Assert.True(false),
+            () => Assert.True(true),
+            () => Assert.True(false));
+      }
+   }
+}
