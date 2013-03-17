cc00f704dcf1c02f9484e220d49e9e6e294adfe0
LukaszK <pabc1122@gmail.com>
2013-03-18 00:41:43 +0100

    Quick add some code for new API generating test case object.

---
+++ QuickTestsFramework/Utils/TestApiAdapter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+//using Microsoft.Test.VariationGeneration;
+
+namespace QuickTestsFramework
+{
+    public sealed class TestApiAdapter : ICombinationarSequecnceGenerator
+    {
+        public IEnumerable<IValueGetter> Sequence(IEnumerable<ParameterSpec> parametersSpec, int order)
+        {
+            /*var p = new List<ParameterBase>();
+            foreach (ParameterSpec parameterSpec in parametersSpec)
+            {
+                var parameterValues = new Parameter<object>(parameterSpec.ParameterName);
+                foreach (object possibleValue in parameterSpec.PossibleValues)
+                    parameterValues.Add(possibleValue);
+
+                p.Add(parameterValues);
+            }
+            var model = new Model(p);
+
+            var valueGetter = new ValueGetter();
+            return model.GenerateVariations(order).Select(variation =>
+            {
+                valueGetter.SetCurr(variation);
+                return valueGetter;
+            });*/
+            return null;
+        }
+
+        /*private sealed class ValueGetter : IValueGetter
+        {
+            private Variation _variation;
+
+            public T GetValue<T>(string key)
+            {
+                return (T)_variation[key];
+            }
+
+            public void SetCurr(Variation variation)
+            {
+                _variation = variation;
+            }
+        }*/
+    }
+}
---
+++ Tests/ObjectGenerator/ObjectGeneratorTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+
+namespace QuickTestsFramework
+{
+    public sealed class A
+    {
+        public int p1;
+        public B p2;
+    }
+
+    public sealed class B
+    {
+        public int p1;
+        public string p2;
+    }
+
+    [TestFixture]
+    public sealed class Class1
+    {
+        [Test]
+        public void T01()
+        {
+            int l = 3;
+            var generator = new ObjectGenerator(new TestApiAdapter());
+            var results = generator.Pairwise(i =>
+                new A
+                {
+                    p1 = One.Of(1, 2 * l, 3),
+                    p2 = new B
+                    {
+                        p1 = One.Of(1, 2) == 1 ? 2 : 3,
+                        p2 = "3"
+                    }
+                }).ToArray();
+
+            Assert.That(results.Length, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void T02()
+        {
+            int l = 3;
+            var generator = new ObjectGenerator(new TestApiAdapter());
+            var results = generator.Pairwise2(i =>
+                new A
+                {
+                    p1 = One.Of(1, 2 * l, 3),
+                    p2 = new B
+                    {
+                        p1 = One.Of(1, 2),
+                        p2 = One.Of("1", "2", "3")
+                    }
+                }).ToArray();
+
+            Assert.That(results.Length, Is.EqualTo(9));
+        }
+    }
+}
