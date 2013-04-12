1bb26b00a62025d69e4a2f256c8a7e058b3c207b
LukaszK <pabc1122@gmail.com>
2013-04-12 19:55:04 +0200

    add empty test

---
+++ Tests/Demo/EmptyTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using QuickTestsFramework;
+
+namespace QuickTestsFramework.Tests
+{
+    [TestFixture]
+    public sealed class EmptyTest
+    {
+        private Runner _runner;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            _runner = RunnerHelper.Create();
+            _runner.RunInitializers(this);
+
+            Console.WriteLine("Process...");
+        }
+
+        [Test]
+        public void T01()
+        {
+            _runner.Run(
+                testCaseGenerator: () => Enumerable.Range(0, 10),
+                inicializer: tc =>
+                {
+                    
+                },
+                assertion: tc =>
+                {
+                    
+                });
+        }
+    }
+}
