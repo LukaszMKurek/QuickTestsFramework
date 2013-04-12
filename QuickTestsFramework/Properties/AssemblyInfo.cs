6d78448c454912dd4cef15cc4a6456b7ced88f34
LukaszK <pabc1122@gmail.com>
2013-04-12 20:27:41 +0200

    some cleanups

--- QuickTestsFramework/Properties/AssemblyInfo.cs
+++ QuickTestsFramework/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QuickTestsFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("QuickTestsFramework.Core")]
+[assembly: AssemblyDescription("Framework to speedup tests for batching systems. Core assembly.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Lukasz K.")]
 [assembly: AssemblyProduct("QuickTestsFramework")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Lukasz K.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
