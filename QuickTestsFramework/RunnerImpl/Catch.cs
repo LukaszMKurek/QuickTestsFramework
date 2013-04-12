2156fa887a00dfdb3fec716c8e9a78f682fad159
LukaszK <pabc1122@gmail.com>
2013-04-12 15:35:41 +0200

    Polish Catching output

--- QuickTestsFramework/RunnerImpl/Catch.cs
+++ QuickTestsFramework/RunnerImpl/Catch.cs
@@ -1,91 +1,96 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Collections.Generic;
-using System;
 
 namespace QuickTestsFramework
 {
-   public static class Catch
-   {
-      private static string ConsoleOutput(Action action)
-      {
-         TextWriter tmp = Console.Out;
-         var sw = new StringWriter();
+    /// <summary>
+    /// Catch and return exception or console, trace, debug output.
+    /// </summary>
+    public static class Catch
+    {
+        /// <summary>
+        /// Name of trace listener that was used during catching output. You may use this information to add some filter. But remember if any listener redirected output from console costom listener won't by add.
+        /// </summary>
+        public static readonly string TraceListenerName = "QuickTestFrameworkTraceListener";
 
-         try
-         {
-            Console.SetOut(sw);
-            action();
-            return sw.ToString();
-         }
-         finally
-         {
-            Console.SetOut(tmp);
-            sw.Dispose();
-         }
-      }
+        /// <summary>
+        /// You can decide to disable caching output from Debug and Trace.
+        /// </summary>
+        public static bool CatchCanHandleTraceAndDebug = true;
 
-      private static void RedirectTraceAndDebugOutputToConsole(Action action)
-      {
-         var writer = new TextWriterTraceListener(Console.Out);
-         try
-         {
-            AddDebugListenerIfNotExist(writer);
-            AddTraceListenerIfNotExist(writer);
+        /// <summary>
+        /// Catch and return Console output in region.
+        /// </summary>
+        public static string ConsoleOutput(Action action)
+        {
+            TextWriter tmp = Console.Out;
+            var sw = new StringWriter();
 
-            action();
-         }
-         finally
-         {
-            RemoveTraceListenerIfExist(writer);
-            RemoveDebugListenerIfExist(writer);
-         }
-      }
+            try
+            {
+                Console.SetOut(sw);
+                action();
+                return sw.ToString();
+            }
+            finally
+            {
+                Console.SetOut(tmp);
+                sw.Dispose();
+            }
+        }
 
-      [Conditional("DEBUG")]
-      private static void AddDebugListenerIfNotExist(TextWriterTraceListener writer)
-      {
-         if (Debug.Listeners.OfType<TextWriterTraceListener>().Any(tw => tw.Writer == Console.Out) == false)
-            Debug.Listeners.Add(writer);
-      }
+        private static void RedirectTraceAndDebugOutputToConsole(Action action)
+        {
+            var writer = new TextWriterTraceListener(Console.Out, TraceListenerName);
+            try
+            {
+                AddTraceListenerIfNotExist(writer);
 
-      [Conditional("DEBUG")]
-      private static void RemoveDebugListenerIfExist(TextWriterTraceListener writer)
-      {
-         Debug.Listeners.Remove(writer);
-      }
+                action();
+            }
+            finally
+            {
+                RemoveTraceListenerIfExist(writer);
+            }
+        }
 
-      [Conditional("TRACE")]
-      private static void AddTraceListenerIfNotExist(TextWriterTraceListener writer)
-      {
-         if (Trace.Listeners.OfType<TextWriterTraceListener>().Any(tw => tw.Writer == Console.Out) == false)
-            Trace.Listeners.Add(writer);
-      }
+        private static void AddTraceListenerIfNotExist(TextWriterTraceListener writer)
+        {
+            if (CatchCanHandleTraceAndDebug && Trace.Listeners.OfType<TextWriterTraceListener>().Any(tw => tw.Writer == Console.Out) == false)
+                Trace.Listeners.Add(writer);
+        }
 
-      [Conditional("TRACE")]
-      private static void RemoveTraceListenerIfExist(TextWriterTraceListener writer)
-      {
-         Trace.Listeners.Remove(writer);
-      }
+        private static void RemoveTraceListenerIfExist(TextWriterTraceListener writer)
+        {
+            Trace.Listeners.Remove(writer);
+        }
 
-      public static string Output(Action action)
-      {
-         return ConsoleOutput(() => RedirectTraceAndDebugOutputToConsole(action));
-      }
+        /// <summary>
+        /// Catch and return console, trace, debug output in region. Depending on confuguration. See:
+        /// </summary>
+        public static string Output(Action action)
+        {
+            return ConsoleOutput(() => RedirectTraceAndDebugOutputToConsole(action));
+        }
 
-      public static Exception Exception(Action action)
-      {
-         try
-         {
-            action();
-         }
-         catch (Exception ex)
-         {
-            return ex;
-         }
+        /// <summary>
+        /// Catch and return exception in region.
+        /// </summary>
+        public static Exception Exception(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
 
-         return null;
-      }
-   }
+            return null;
+        }
+    }
 }
