cf708e25e1abfff04a3a5d5e61ccfc5c29145505
LukaszK <pabc1122@gmail.com>
2013-04-12 16:33:47 +0200

    Fix problem when optimizations are on.

--- QuickTestsFramework.NUnitUniwersal/NUnitTestMethodSelectorFromCallStack.cs
+++ QuickTestsFramework.NUnitUniwersal/NUnitTestMethodSelectorFromCallStack.cs
@@ -6,27 +6,28 @@ using System.Reflection;
 
 namespace QuickTestsFramework.NUnit
 {
-   public sealed class NUnitTestMethodSelectorFromCallStack : ITestMethodSelectorFromCallStack
-   {
-      //global::NUnit.Framework.TestContext.CurrentContext.Test.FullName
-      public MethodBase GetCallingTestMethod()
-      {
-         StackFrame[] stackFrames = new StackTrace().GetFrames();
-         if (stackFrames == null)
-            throw new InvalidOperationException("Reflection sucks. Probably you wrong use Run method.");
+    public sealed class NUnitTestMethodSelectorFromCallStack : ITestMethodSelectorFromCallStack
+    {
+        //global::NUnit.Framework.TestContext.CurrentContext.Test.FullName
+        public MethodBase GetCallingTestMethod()
+        {
+            StackFrame[] stackFrames = new StackTrace().GetFrames();
+            if (stackFrames == null)
+                throw new InvalidOperationException("Reflection sucks. Probably you wrong use Run method.");
 
-         StackFrame stackFrame = stackFrames.FirstOrDefault(x =>
-         {
-            var methodBase = x.GetMethod();
-            return
-               methodBase.IsDefined(NUnitHelper.GetType("NUnit.Framework.TestAttribute"), true) ||
-               methodBase.IsDefined(NUnitHelper.GetType("NUnit.Framework.TestCaseSourceAttribute"), true) ||
-               methodBase.IsDefined(NUnitHelper.GetType("NUnit.Framework.TestCaseAttribute"), true);
-         });
-         if (stackFrame == null)
-            throw new InvalidOperationException("Run method can be call only by test method with [Test] attribute.");
+            StackFrame stackFrame = stackFrames.FirstOrDefault(x =>
+            {
+                MethodBase methodBase = x.GetMethod();
+                return
+                    methodBase.IsDefined(NUnitHelper.GetType("NUnit.Framework.TestAttribute"), true) ||
+                    methodBase.IsDefined(NUnitHelper.GetType("NUnit.Framework.TestCaseSourceAttribute"), true) ||
+                    methodBase.IsDefined(NUnitHelper.GetType("NUnit.Framework.TestCaseAttribute"), true);
+            });
+            if (stackFrame == null)
+                throw new InvalidOperationException(
+                    "Run method can be call only by test method with [Test] attribute and the test project must have disabled IL optimization (innlining was main problem).");
 
-         return stackFrame.GetMethod();
-      }
-   }
+            return stackFrame.GetMethod();
+        }
+    }
 }
