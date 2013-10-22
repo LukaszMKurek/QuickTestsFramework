225ea2b5022ec2fbdc80b93dc8ff2fbc4257d335
LukaszK <pabc1122@gmail.com>
2013-10-22 17:33:45 +0200

    MsTest description fix

--- QuickTestsFramework.MSTest/Properties/AssemblyInfo.cs
+++ QuickTestsFramework.MSTest/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("QuickTestsFramework.MSTest")]
-[assembly: AssemblyDescription("Add integration with NUnit for QuickTestsFramework.")]
+[assembly: AssemblyDescription("Add integration with MSTest for QuickTestsFramework.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lukasz K.")]
 [assembly: AssemblyProduct("QuickTestsFramework")]
